22caaf60edadc2748855c2ff8e6b13459b8b4b90
JoseAugustoDosSantos <augusto03jose@gmail.com>
2026-03-13 12:19:17 -0300

    incluindo cognito e s3

--- locals.tf
+++ locals.tf
@@ -1,10 +1,10 @@
 data "aws_availability_zones" "available" {}
 
 locals {
-  region             = var.aws_region
-  name               = "${var.local_name.name}-${var.local_name.env}"
-  vpc_cidr           = var.vpc_cidr_block
-  azs                = slice(data.aws_availability_zones.available.names, 0, var.vpc_subnets_count)
+  region   = var.aws_region
+  name     = "${var.local_name.name}-${var.local_name.env}"
+  vpc_cidr = var.vpc_cidr_block
+  azs      = slice(data.aws_availability_zones.available.names, 0, var.vpc_subnets_count)
   tags = {
     Terraform = "true",
     Name      = var.local_name.name,
--- variables.tf
+++ variables.tf
@@ -24,3 +24,28 @@ variable "vpc_subnets_count" {
   type        = number
   default     = 2 # For EKS, you need at least two availability zones??
 }
+
+
+variable "project_name" {
+  description = "Project prefix"
+  type        = string
+  default     = "cognito-api-auth"
+}
+
+variable "enable_user_pool_domain" {
+  description = "Create a Cognito domain for hosted UI (optional)"
+  type        = bool
+  default     = false
+}
+
+variable "user_pool_domain_prefix" {
+  description = "Cognito domain prefix (if enable_user_pool_domain = true)"
+  type        = string
+  default     = "my-auth-domain"
+}
+
+#ELB uri
+variable "services" {
+  type    = map(string)
+  default = {}
+}
